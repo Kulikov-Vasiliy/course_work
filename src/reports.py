1365234717b17091dedbed4e6de6bcd1dda529f3
Василий <kulikov821999@gmail.com>
2025-10-22 22:21:15 +0300

    deleted comments, unused lines, changed write to file method

--- src/reports.py
+++ src/reports.py
@@ -2,16 +2,14 @@ import datetime
 
 import logging
 
+import numpy as np
 from dateutil.relativedelta import relativedelta
 
 from typing import Optional
 
 import pandas as pd
 
-import numpy as np
-
-from pyexcelerate import Workbook
-
+import xlsxwriter
 
 file_path_param = (
     "C:/Users/Я/Desktop/ДЛЯ РАБОТЫ/pythonProject/course_work/"
@@ -58,7 +56,6 @@ def spending_by_category(transactions: pd.DataFrame,
             df["Дата операции"] = pd.to_datetime(
                 df["Дата операции"], format="%d.%m.%Y %H:%M:%S"
             )
-            resulted = pd.DataFrame()
             logger.info("фильтрация дат и очистка категорий от пустых значений")
             day = df["Дата операции"].dt.day
             month = df["Дата операции"].dt.month
@@ -67,115 +64,64 @@ def spending_by_category(transactions: pd.DataFrame,
                 & (df["Дата операции"].dt.day == day)
                 ]
             filtered_data = filtered_data.dropna(subset=["Категория"])
+
             logger.info("получение точки начала периода")
             if filtered_data["Дата операции"].dt.day.isin(day).any():
-            # if day in filtered_data["Дата операции"].dt.day:
                 filtered_data_start = df[
-                    df["Дата операции"].apply(lambda x: x - relativedelta(months=3)).dt.month == month - 3
+                    df["Дата операции"].apply(lambda x: x - relativedelta(months=3)).dt.month
                     & (df["Дата операции"].dt.day == day)
                     ]
             else:
                 month_offset = datetime.datetime.today() - relativedelta(months=3)
                 current_day = datetime.datetime.today()
                 filtered_data_start = df[
-                    df["Дата операции"].apply(lambda x: x - relativedelta(months=3)).dt.month == month_offset.month
+                    df["Дата операции"].apply(lambda x: month_offset.month)
                     & (df["Дата операции"].dt.day == current_day)
                     ]
 
             logger.info("подбор необходимых данных")
-            to_result = filtered_data_start[
-                filtered_data_start.notna().any(axis=0) &  # Проверка на ненулевые значения во всех колонках
+            filter_result = filtered_data_start[
+                filtered_data_start.notna().any(axis=1) &  # Проверка на ненулевые значения во всех колонках
                 (filtered_data_start["Сумма операции"] < 0) &  # Сумма операции отрицательна
                 (filtered_data_start["Категория"] == category)  # Категория равна заданной
                 ]
-            resulted = to_result.copy()
-            resulted.dropna(inplace=True)
-            resulted = to_result[
-                    ["Номер карты",
+            filter_result = filter_result.copy()
+            resulted = filter_result[
+                    ["Дата платежа",
+                    "Номер карты",
                     "Статус",
                     "Сумма операции",
-                    "Кэшбэк",
+                    "Кэшбэк" if "Кэшбэк" else abs(filter_result["Сумма операции"]) // 100,
                     "MCC",
+                    "Категория",
                     "Описание",
                     "Округление на инвесткопилку",
-                    "Бонусы (включая кэшбэк)",
-                    ]].values.tolist()
-            # resulted.dropna(inplace=True)
-
-            print(resulted)
+                    "Бонусы (включая кэшбэк)" if "Бонусы (включая кэшбэк)" else abs(filter_result["Сумма операции"]) // 100,
+                    ]]
+            resulted_cleaned = resulted.replace([np.inf, -np.inf], np.nan).fillna(0)
 
             logger.info("формирование файла")
-            wb = Workbook()
-            wb.new_sheet(sheet_name="отфильтровано по категории", data=resulted)
-            wb.save(r"C:/Users/Я/Desktop/ДЛЯ РАБОТЫ/pythonProject/course_work/"
+            workbook = xlsxwriter.Workbook("C:/Users/Я/Desktop/ДЛЯ РАБОТЫ/pythonProject/course_work/"
                             "pythonProject/data/transactions.xlsx")
+            worksheet = workbook.add_worksheet()
+            for col_num, col_data in enumerate(resulted_cleaned.columns):
+                worksheet.set_column(col_num, col_num, 50)
+
+            # Запись заголовков
+            for col_num, col_name in enumerate(resulted_cleaned.columns):
+                worksheet.write(0, col_num, col_name)
+
+            # Запись данных
+            for row_num, row_data in enumerate(resulted_cleaned.values):
+                worksheet.write_row(row_num + 1, 0, row_data)
+
+            workbook.close()
 
             return result
 
         return wrapper
 
     return decorator
-
-                # for index, row in df.iterrows():
-            #     day = row["Дата операции"].day
-            #     month = row["Дата операции"].month
-            #     filtered_data = df[
-            #         (df["Дата операции"].dt.month == month)
-            #         & (df["Дата операции"].dt.day == day)
-            #         ]
-            #     filtered_data = filtered_data.dropna(subset=["Категория"])
-
-
-                # if day in filtered_data["Дата операции"].dt.day:
-                #     filtered_data_start = df[
-                #         df["Дата операции"].apply(lambda x: x - relativedelta(months=3)).dt.month == month - 3
-                #         & (df["Дата операции"].dt.day == day)
-                #         ]
-                # else:
-                #     month_offset = datetime.datetime.today() - relativedelta(months=3)
-                #     current_day = datetime.datetime.today()
-                #     filtered_data_start = df[
-                #         df["Дата операции"].apply(lambda x: x - relativedelta(months=3)).dt.month == month_offset.month
-                #         & (df["Дата операции"].dt.day == current_day)
-                #         ]
-
-
-                # for i, el in filtered_data_start.iterrows():
-                #     if pd.isna(
-                #             el["Номер карты"].strip()
-                #             or el["Категория"]
-                #             or el["Описание"]
-                #             or el["Дата платежа"]
-                #             or el["Статус"]
-                #             or el["Сумма операции"] and el["Сумма операции"] >= 0
-                #             or el["Кэшбэк"]
-                #             or el["MCC"]
-                #             or el["Округление на инвесткопилку"]
-                #             or el["Бонусы (включая кэшбэк)"]
-                #     ):
-                #         continue
-
-    #                 if el["Категория"] == category:
-    #                     resulted = filtered_data[
-    #                         ["Номер карты",
-    #                          "Статус",
-    #                          "Сумма операции",
-    #                          "Кэшбэк",
-    #                          "MCC",
-    #                          "Описание",
-    #                          "Округление на инвесткопилку",
-    #                          "Бонусы (включая кэшбэк)",
-    #                          ]]
-    #
-    #         logger.info("формирование файла")
-    #         wb = Workbook()
-    #         wb.new_sheet(data=resulted)
-    #         wb.save(r"C:/Users/Я/Desktop/ДЛЯ РАБОТЫ/pythonProject/course_work/"
-    #                 "pythonProject/data/transactions.xlsx", index=False, header=True)
-    #
-    #     return wrapper
-    #
-    # return decorator
 
 
 @spending_by_category(file_path_param, "Аптеки", "2021-12-30 08:16:00")
